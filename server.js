3f1ed9a340a32ccc6ec9bdc968a26001d298abf2
sh3ngsh3ng <ysdeveloper01@gmail.com>
2023-12-14 22:27:45 +0800

    simple express server for local app testing

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require('cors')
+
+let app = express();
+
+app.use(cors())
+
+async function main() {
+  app.get("/questions", (req,res) => {
+    let result = [
+        {
+            'prompt': 'Is this question 1?',
+            'options': ['Yes' , 'No'],
+            'ans': 'Yes',
+            'qnTyp': 'mcq'
+        }
+    ]
+    return res.json(result)
+  })
+
+  app.get('/rejected-promise', (req,res) => {
+    const error = new Error('Rejected Promise.')
+    return Promise.reject(error)
+  })
+
+
+
+}
+
+main();
+
+app.listen(4000, () => {
+  console.log("Server has started");
+});
